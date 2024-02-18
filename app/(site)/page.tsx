5efc260c03a7041211a6c3e31e5ed16b3a66b92a
Habeeb Yunus Habeeb <habeebdh1@outlook.com>
2024-02-18 12:28:45 +0100

    Remove unused codes

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-8xl text-blue-500 font-semibold text-center tracking-widest my-5">
+        Messenger Clone
+      </h1>
+    </main>
+  );
+}
